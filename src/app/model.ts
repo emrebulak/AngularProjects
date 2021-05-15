99f9c78535acc546e8dbf1778ac852d8a84d352e
emrebulak <49715308+emrebulak@users.noreply.github.com>
2021-05-15 17:09:16 +0300

    AnguarProject_ToDo

---
+++ src/app/model.ts
@@ -0,0 +1,24 @@
+export class Model {
+    user;
+    items: any;
+    constructor() {
+        this.user = "Baran Emre";
+        this.items = [
+            new TodoItem("Spor", true),
+            new TodoItem("Kahvaltı", false),
+            new TodoItem("Kitap Okumak", false),
+            new TodoItem("Sinema", false),
+        ];
+
+    }
+};
+
+export class TodoItem {
+    description;
+    action;
+
+    constructor(description: any, action: any) {
+        this.description = description;
+        this.action = action;
+    }
+};
